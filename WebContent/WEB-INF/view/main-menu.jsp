6b8ceba1c2fb8018fc061938b0852c57cf34bc3f
unknown <swest06@dcs.bbk.ac.uk>
2020-11-13 11:25:03 +0000

    update

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -5,5 +5,6 @@
 
 <a href="hello/showForm">Hello world form</a>
 <a href="student/showForm">Student form</a>
+<a href="customer/showForm">Customer form</a>
 </body>
 </html>
---
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -0,0 +1,47 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// Init binder to pre-process data coming into controller
+	// Will trim leading & trailing whitespace from strings
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		// true resolves the string to null if it's all whitespace
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm(Model model) {
+		
+		model.addAttribute("customer", new Customer());
+		
+		return "customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "customer-form";
+		} else {
+			return "customer-confirmation";
+		}
+	}
+}
